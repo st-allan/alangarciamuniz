072586c5a01c270045a013e321a7f4d2c56a3ce2
Alan Garcia <83896331+st-allan@users.noreply.github.com>
2024-07-04 10:52:23 -0700

    Change note format

---
+++ _plugins/spanish_date.rb
@@ -0,0 +1,18 @@
+module Jekyll
+    module SpanishDateFilter
+      MONTHS = {
+        1 => 'enero', 2 => 'febrero', 3 => 'marzo', 4 => 'abril',
+        5 => 'mayo', 6 => 'junio', 7 => 'julio', 8 => 'agosto',
+        9 => 'septiembre', 10 => 'octubre', 11 => 'noviembre', 12 => 'diciembre'
+      }
+  
+      def spanish_date(date)
+        day = date.strftime("%-d")
+        month = MONTHS[date.strftime("%-m").to_i]
+        year = date.strftime("%Y")
+        "#{day} de #{month} de #{year}"
+      end
+    end
+  end
+  
+  Liquid::Template.register_filter(Jekyll::SpanishDateFilter)
