67b46cd73ac2f3d31f7cbc8770ca8a5352f51bcf
Alan Garcia <83896331+st-allan@users.noreply.github.com>
2024-08-26 17:43:00 -0700

    Add reading time filter in note layut

---
+++ _plugins/reading_time_filter.rb
@@ -0,0 +1,12 @@
+module Jekyll
+    module ReadingTimeFilter
+      def reading_time(input)
+        words_per_minute = 200
+        words = input.split.size
+        minutes = (words / words_per_minute.to_f).ceil
+        "#{minutes} min en leer"
+      end
+    end
+  end
+  
+  Liquid::Template.register_filter(Jekyll::ReadingTimeFilter)
