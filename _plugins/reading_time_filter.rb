7630a7d17a38c75635a0bffc9cb420e9a5ab10de
Alan Garcia <83896331+st-allan@users.noreply.github.com>
2024-08-26 17:46:37 -0700

    Add new text to reading time filter

--- _plugins/reading_time_filter.rb
+++ _plugins/reading_time_filter.rb
@@ -4,7 +4,7 @@ module Jekyll
         words_per_minute = 200
         words = input.split.size
         minutes = (words / words_per_minute.to_f).ceil
-        "#{minutes} min en leer"
+        "#{minutes} min de lectura"
       end
     end
   end
